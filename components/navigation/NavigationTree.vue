2e95514b50d0129bc3d91a1b963a6ebe0e335abe
alptangal <alptangal@gmail.com>
2024-06-19 21:36:02 +0700

    first commit

---
+++ components/navigation/NavigationTree.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+
+const items = [{
+  label: 'Getting Started',
+  icon: 'i-heroicons-information-circle',
+  defaultOpen: true,
+  items:[
+    {label:'home',to:'/'},
+    {label:'about',to:'/about'},
+    {label:'contact',to:'/contact'}
+  ]
+}, {
+  label: 'Installation',
+  icon: 'i-heroicons-arrow-down-tray',
+  disabled: true,
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
+}, {
+  label: 'Theming',
+  icon: 'i-heroicons-eye-dropper',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
+}, {
+  label: 'Layouts',
+  icon: 'i-heroicons-rectangle-group',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
+}, {
+  label: 'Components',
+  icon: 'i-heroicons-square-3-stack-3d',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
+}, {
+  label: 'Utilities',
+  icon: 'i-heroicons-wrench-screwdriver',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
+}]
+const colorMode = useColorMode()
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1 class="text-center py-5">PDM</h1>
+    <UAccordion :items="items" color="secondary">
+      <template #item="{item}" >
+        <h3 v-if="item.content">{{item.content}}</h3>
+        <template v-else class="flex flex-column">
+          <ULink class="block border-l border-slate-600 hover:border-slate-200 pl-4" v-for="itc in item.items" :to="itc.to">{{ itc.label }}</ULink>
+        </template>
+      </template>
+    </UAccordion>
+  </div>
+    
+    
+  
+</template>
+
---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-content-center">
+    <UContainer class="flex flex-row" :ui="ui">
+        <NavigationTree  class="w-80 min-w-12 max-w-80"/>
+      <UDivider orientation="vertical" />
+      <UCard :ui="{background:'bg-transparent',shadow:'shadow-none',ring:'ring-0',rounded:'rounded-none',body:{padding:'sm:px-2.5 sm:py-1.5'}}" class="w-full">
+        <slot />
+      </UCard>
+      
+    </UContainer>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+import NavigationTree from '~/components/navigation/NavigationTree.vue';
+const ui=ref({
+  constrained:'max-w-full w-full',
+  padding:'px-0 lg:px-0',
+  base:'text-xl'
+})
+
+
+</script>
+
+<style>
+
+</style>
