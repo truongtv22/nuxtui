f22d01c9fb124206db098d9cb4fc00595b177a9d
alptangal <alptangal@gmail.com>
2024-07-01 10:32:49 +0700

    .

--- components/administrator/warehouse/CreateNew.vue
+++ components/administrator/warehouse/CreateNew.vue
@@ -448,6 +448,46 @@ const options = {
   "useGrouping": true,
   "accountingSign": true
 }
+const result = ref('')
+const error = ref('')
+function onDetect(detectedCodes) {
+    result.value = JSON.stringify(
+      detectedCodes.map(code => code.rawValue)
+    )
+  }
+  function onError(err) {
+    error.value = `[${err.name}]: `
+
+    if (err.name === 'NotAllowedError') {
+      error.value += 'you need to grant camera access permission'
+    } else if (err.name === 'NotFoundError') {
+      error.value += 'no camera on this device'
+    } else if (err.name === 'NotSupportedError') {
+      error.value += 'secure context required (HTTPS, localhost)'
+    } else if (err.name === 'NotReadableError') {
+      error.value += 'is the camera already in use?'
+    } else if (err.name === 'OverconstrainedError') {
+      error.value += 'installed cameras are not suitable'
+    } else if (err.name === 'StreamApiNotSupportedError') {
+      error.value += 'Stream API is not supported in this browser'
+    } else if (err.name === 'InsecureContextError') {
+      error.value += 'Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.'
+    } else {
+      error.value += err.message
+    }
+  }
+  function paintBoundingBox(detectedCodes, ctx) {
+    for (const detectedCode of detectedCodes) {
+      const {
+        boundingBox: { x, y, width, height }
+      } = detectedCode
+
+      ctx.lineWidth = 2
+      ctx.strokeStyle = '#007bff'
+      ctx.strokeRect(x, y, width, height)
+    }
+  }
+
 </script>
 
 <template>
@@ -476,7 +516,7 @@ const options = {
         <UForm :schema="schema" :state="productInfo" class="space-y-4" @submit="onSubmit">
           <UFormGroup label="Tên sản phẩm" name="name">
             <ClientOnly >
-              <QrcodeStream camera="auto"> </QrcodeStream>
+              <QrcodeStream camera="auto" :track="paintBoundingBox" @detect="onDetect" @error="onError"> </QrcodeStream>
             </ClientOnly>
             
             <div v-if="productInfo.selected.length > 0"
