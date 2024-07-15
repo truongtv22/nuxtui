46ac7c8f2af8ca676f5f36c1fb6a4e7c44022dd7
alptangal <alptangal@gmail.com>
2024-07-15 13:07:15 +0700

    .

--- pages/test.vue
+++ pages/test.vue
@@ -1,14 +1,26 @@
 <template>
   <h1>hello world</h1>
+  <video ref="vid"/>
 </template>
 <script setup>
+const vid=ref(null)
 onMounted(async ()=>{
-  let stream = null;
-  try {
-    stream = await navigator.mediaDevices.getUserMedia(constraints);
-    /* use the stream */
-  } catch (err) {
-    /* handle the error */
-  }
+  const constraints = {
+  audio: true,
+  video: { width: 1280, height: 720 },
+};
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then((mediaStream) => {
+    video.value.srcObject = mediaStream;
+    video.value.onloadedmetadata = () => {
+      video.value.play();
+    };
+  })
+  .catch((err) => {
+    // always check for errors at the end.
+    console.error(`${err.name}: ${err.message}`);
+  });
 })
 </script>
