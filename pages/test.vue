49a9438562c40c77909a9d92e3af51da02b1e158
alptangal <alptangal@gmail.com>
2024-07-15 16:03:31 +0700

    .

--- pages/test.vue
+++ pages/test.vue
@@ -3,7 +3,18 @@
     <h1>heloo wol</h1>
     <barcode-scanner  class="barcode-scanner" 
                      :source="camera" 
-                     @bcs-scanned="scanned" ></barcode-scanner>
+                     @bcs-scanned="scanned" >
+                     <video
+          ref="videoSource"
+          crossorigin="anonymous"
+          muted
+          autoplay
+          playsinline
+          controls
+          controlslist="nodownload nofullscreen"
+        >
+        </video>
+                    </barcode-scanner>
                      <h3>Detected barcodes</h3>
         
     <ul>
@@ -15,12 +26,16 @@
 
   <script setup>
 const camera=ref(null)
+const videoSource=ref(null)
 const barcodes=ref([])
 function scanned(barcodes) {
       barcodes.value = barcodes
     }
 onMounted(()=>{
   navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-      .then(stream => camera.value = stream)
+      .then(stream => {
+        camera.value = stream
+        videoSource.value=stream
+      })
 })
 </script>
