47be9a596988d18da8859cbe89eb082770a50cb2
phuongDoMega <pdmega268@gmail.com>
2024-07-27 22:24:20 +0700

    .

--- pages/administrator/categories/index.vue
+++ pages/administrator/categories/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-between px-3 py-3.5 border-b border-gray-200 dark:border-gray-700 relative">
     <div class="flex gap-1">
-      <UButton class="capitalize" :ui="{ rounded: 'rounded-full' }" @click="modals.createForm.display = true" >
+      <UButton class="capitalize" :ui="{ rounded: 'rounded-full' }" @click="modals.createForm.display = true">
         <UIcon class="font-bold text-xl" name="i-material-symbols-light-add" />Create new category
       </UButton>
       <Transition>
@@ -27,17 +27,18 @@
     </div>
     <UInput v-model="table.keyword" placeholder="Enter to search" />
   </div>
-
-  <UTable :rows="filteredRows" :columns="table.columns" v-model="table.selected" :loading="table.loading" :ui="{base:'w-full'}">
+  <div ref="tableEl">
+    <UTable :rows="filteredRows" :columns="tableColumns" v-model="table.selected" :loading="table.loading"
+    :ui="{ base:basicStore.screenSize.name=='xs'?'w-full':'',tr:{base:basicStore.screenSize.name=='xs'?'flex items-center':''}, td: { base: basicStore.screenSize.name == 'xs' ? 'truncate' : '' } }">
     <template #actions-data="{ row }">
       <UButton color="blue" icon="i-material-symbols-light-info-outline-rounded" variant="ghost"
         :ui="{ rounded: 'rounded-full' }" @click="table.detail = { display: true, value: row }" />
     </template>
-    <template #title="{row}">
+    <template #title="{ row }">
       <div class="w-full">{{ row.title }}</div>
-      </template>
+    </template>
     <template #images-data="{ row }">
-      <div class="relative flex justify-center" @dblclick="router.push('categories/detail-'+row._id)">
+      <div class="relative flex justify-center" @dblclick="router.push('categories/detail-' + row._id)">
         <template v-if="row.images.small.length >= 1">
           <div class="w-12 h-12 bg-gray-400 rounded-md" v-if="row.images.small.length > 1"></div>
           <div :class="row.images.small.length > 1 ? 'absolute top-0 left-2 shadow-2xl' : ''">
@@ -68,47 +69,50 @@
       </span>
     </template>
   </UTable>
+  </div>
   <UDivider class="mb-2" />
   <div class="flex justify-center mt-auto mb-2 w-full">
     <UPagination v-model="table.page" :page-count="pageCount" :total="table.data.length" />
   </div>
 
   <KeepAlive>
-    <UModal :ui="{ width: `sm:max-w-6xl`, overlay: { background: 'backdrop-blur-md' } }" v-model="modals.createForm.display"
-    :fullscreen="basicStore.screenSize.w < 800 ? true : false" prevent-close>
-    <UCard :ui="{
-      base: 'h-fit flex flex-col',
-      rounded: '',
-      divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-      body: {
-        base: 'grow'
-      },
-      header: {
-        base: 'bg-green-500 relative'
-      }
-    }" class="relative">
-      <template #header>
-        <div class="flex items-center justify-between">
-          <UBadge color="green" class="absolute -top-4 left-0 hidden xl:block">Create new</UBadge>
-          <h3 class="capitalize text-base font-semibold leading-6 text-white dark:text-white">
-            tạo mới thể loại
-          </h3>
-          <UButton :disabled="loading.doing" color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
-            @click=" modals.confirmClose.display = true, modals.confirmClose.title = 'Bạn có chắc muốn đóng cửa sổ này?'" />
-        </div>
-        <div class="bottom-0 absolute w-full right-0" v-if="loading.doing">
-          <UProgress size="xs" animation="carousel" :ui="{progress:{rounded:'rounded-none'}}" />
-        </div>
-      </template>
-    <AdministratorCategoriesCreateNew
-      @newData="table.data.unshift($event), store.showNotification({ type: 'success', title: $event.title + ' created success', description: 'You can view in data table', timeout: 3000 })" @doing="loading.doing=$event"/>
+    <UModal :ui="{ width: `sm:max-w-6xl`, overlay: { background: 'backdrop-blur-md' } }"
+      v-model="modals.createForm.display" :fullscreen="basicStore.screenSize.w < 800 ? true : false" prevent-close>
+      <UCard :ui="{
+        base: 'h-fit flex flex-col',
+        rounded: '',
+        divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+        body: {
+          base: 'grow'
+        },
+        header: {
+          base: 'bg-green-500 relative'
+        }
+      }" class="relative">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <UBadge color="green" class="absolute -top-4 left-0 hidden xl:block">Create new</UBadge>
+            <h3 class="capitalize text-base font-semibold leading-6 text-white dark:text-white">
+              tạo mới thể loại
+            </h3>
+            <UButton :disabled="loading.doing" color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click=" modals.confirmClose.display = true, modals.confirmClose.title = 'Bạn có chắc muốn đóng cửa sổ này?'" />
+          </div>
+          <div class="bottom-0 absolute w-full right-0" v-if="loading.doing">
+            <UProgress size="xs" animation="carousel" :ui="{ progress: { rounded: 'rounded-none' } }" />
+          </div>
+        </template>
+        <AdministratorCategoriesCreateNew
+          @newData="table.data.unshift($event), store.showNotification({ type: 'success', title: $event.title + ' created success', description: 'You can view in data table', timeout: 3000 })"
+          @doing="loading.doing = $event" />
       </UCard>
-  </UModal>
-    
+    </UModal>
+
   </KeepAlive>
   <ConfirmModal v-model="modals.confirmDelete.display" :title="modals.confirmDelete.title"
     @is-confirmed="$event ? deleteSelected(null) : loading.delete = false" />
-    <ConfirmModal v-model="modals.confirmDeleteAll.display" :title="modals.confirmDeleteAll.title"
+  <ConfirmModal v-model="modals.confirmDeleteAll.display" :title="modals.confirmDeleteAll.title"
     @is-confirmed="$event ? deleteSelected('all') : loading.delete = false" />
   <ConfirmModal v-model="modals.confirmClose.display" :title="modals.confirmClose.title"
     @is-confirmed="$event ? modals.createForm.display = false : null" />
@@ -135,20 +139,22 @@
             @click="table.detail.display = false" />
         </div>
         <div class="bottom-0 absolute w-full right-0" v-if="loading.doing">
-          <UProgress size="xs" color="blue" animation="carousel" :ui="{progress:{rounded:'rounded-none'}}" />
+          <UProgress size="xs" color="blue" animation="carousel" :ui="{ progress: { rounded: 'rounded-none' } }" />
         </div>
       </template>
-      <AdministratorCategoriesDetail :data="table.detail.value" @updateData="updateData" @doing="loading.doing=$event"/>
+      <AdministratorCategoriesDetail :data="table.detail.value" @updateData="updateData"
+        @doing="loading.doing = $event" />
     </UCard>
   </UModal>
 </template>
 
 <script lang="ts" setup>
-const router=useRouter()
-const route=useRoute()
+const router = useRouter()
+const route = useRoute()
 const store = useMyNotificationsStore()
 const basicStore = useMyBasicStore()
 onBeforeMount(async () => {
+  
   table.value.loading = true
   await $fetch('/api/categories/list').then(res => {
     if (res.length > 0) {
@@ -156,10 +162,9 @@ onBeforeMount(async () => {
     }
     table.value.loading = false
   })
-  if(basicStore.screenSize.name=='xs'){
-    table.value.columns=[{ key: 'images', label: 'Images' },
-    { key: 'title', label: 'Name' }]
-  }
+})
+onMounted(()=>{
+  
 })
 function reformatDate(val) {
   return new Date(Date.parse(val))
@@ -185,21 +190,31 @@ const modals = ref({
 const loading = ref({
   delete: false,
   create: false,
-  doing:false,
+  doing: false,
 })
+const tableColumns = computed(() => {
+  switch (basicStore.screenSize.name) {
+    default:
+      return [
+        { key: '_id', label: 'ID' },
+        { key: 'images', label: 'Images' },
+        { key: 'title', label: 'Name' },
+        { key: 'created_at', label: 'Created at' },
+        { key: 'created_by', label: 'Created by' },
+        { key: 'edited_at', label: 'Edited at' },
+        { key: 'edited_by', label: 'Edited by' },
+        { key: 'actions' }
+      ]
+    case 'xs':
+      return [
+        { key: 'title', label: 'Name' },
+      ]
+  }
+})
+const tableEl=ref(null)
 const table = ref({
   page: 1,
   pageCount: 10,
-  columns: [
-    { key: '_id', label: 'ID' },
-    { key: 'images', label: 'Images' },
-    { key: 'title', label: 'Name' },
-    { key: 'created_at', label: 'Created at' },
-    { key: 'created_by', label: 'Created by' },
-    { key: 'edited_at', label: 'Edited at' },
-    { key: 'edited_by', label: 'Edited by' },
-    { key: 'actions' }
-  ],
   selected: [],
   keyword: null,
   data: [],
@@ -213,6 +228,11 @@ const rows = computed(() => {
   return table.value.data.slice((table.value.page - 1) * pageCount.value, (table.value.page) * pageCount.value)
 })
 const pageCount = computed(() => {
+  if(tableEl.value ){
+    console.log(tableEl.value.offsetTop)
+    return Math.floor((basicStore.screenSize.h-tableEl.value.offsetTop-32-68.5) / 80.5)
+  }
+  
   return Math.floor((basicStore.screenSize.h - 84 - 61 - 48.5 - 32) / 65)
 
 })
@@ -228,10 +248,10 @@ const filteredRows = computed(() => {
   })
 })
 async function deleteSelected(type) {
-  loading.value.delete=true
+  loading.value.delete = true
   const prom = new Promise(async (resolve1, reject1) => {
-    if(type=='all'){
-      table.value.selected=table.value.data
+    if (type == 'all') {
+      table.value.selected = table.value.data
     }
     const arr = Array.from(Array(table.value.selected.length).keys())
     for (const index of arr) {
@@ -247,8 +267,8 @@ async function deleteSelected(type) {
             resolve(table.value.data.findIndex(r => r._id == item._id))
           })
           promise.then(rs => {
-            table.value.data=table.value.data.filter(item1=>item._id!=item1._id)
-            table.value.selected=table.value.selected.filter(item1=>item1._id!=item._id)
+            table.value.data = table.value.data.filter(item1 => item._id != item1._id)
+            table.value.selected = table.value.selected.filter(item1 => item1._id != item._id)
             store.showNotification({
               title: `${item._id} deleted success`,
               description: `with name: <span class="text-red-500 font-bold text-xl">${item.title}</span>`,
@@ -292,30 +312,30 @@ function updateData(data) {
     })
   })
 }
-const modalCreateDisplay=computed({
-  get(){
+const modalCreateDisplay = computed({
+  get() {
     return modals.value.createForm.display
   }
 })
-watch(modalCreateDisplay,(newVal,oldVal)=>{
-  if(newVal){
-    window.history.pushState({},null,route.fullPath+'/create')
+watch(modalCreateDisplay, (newVal, oldVal) => {
+  if (newVal) {
+    window.history.pushState({}, null, route.fullPath + '/create')
   }
-  else{
-    window.history.pushState({},null,route.fullPath)
+  else {
+    window.history.pushState({}, null, route.fullPath)
   }
 })
-const modalUpdateDisplay=computed({
-  get(){
+const modalUpdateDisplay = computed({
+  get() {
     return table.value.detail.display
   }
 })
-watch(modalUpdateDisplay,(newVal,oldVal)=>{
-  if(newVal){
-    window.history.pushState({},null,route.fullPath+'/detail-'+table.value.detail.value._id)
+watch(modalUpdateDisplay, (newVal, oldVal) => {
+  if (newVal) {
+    window.history.pushState({}, null, route.fullPath + '/detail-' + table.value.detail.value._id)
   }
-  else{
-    window.history.pushState({},null,route.fullPath)
+  else {
+    window.history.pushState({}, null, route.fullPath)
   }
 })
 </script>
